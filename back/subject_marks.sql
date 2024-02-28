e5f9041ab9faedfd5181bd77d7adc541ca47ed94
Treston <trestonandrade17@gmail.com>
2024-02-28 20:42:17 +0530

    table structure

---
+++ back/subject_marks.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 28, 2024 at 04:10 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject_marks`
+--
+
+CREATE TABLE `subject_marks` (
+  `enrollment_number` int(11) DEFAULT NULL,
+  `semester` int(11) DEFAULT NULL,
+  `year` smallint(6) NOT NULL,
+  `month` varchar(10) NOT NULL,
+  `sub_code` varchar(10) NOT NULL,
+  `sub_name` varchar(150) NOT NULL,
+  `theory_marks` decimal(5,2) DEFAULT NULL,
+  `term_marks` decimal(5,2) DEFAULT NULL,
+  `practicals` decimal(5,2) DEFAULT NULL,
+  `term_work` decimal(5,2) DEFAULT NULL,
+  `total_marks` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `subject_marks`
+--
+ALTER TABLE `subject_marks`
+  ADD PRIMARY KEY (`sub_code`),
+  ADD KEY `enrollment_number` (`enrollment_number`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `subject_marks`
+--
+ALTER TABLE `subject_marks`
+  ADD CONSTRAINT `subject_marks_ibfk_1` FOREIGN KEY (`enrollment_number`) REFERENCES `personal_details` (`enrollment_number`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
