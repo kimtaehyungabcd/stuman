59fce1bc2728c0faa710ef4cc53cbddcd43da005
GTX18 <abhishekhaldankar18@gmail.com>
2024-05-10 13:16:03 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # stuman
+
+install xampp
+install nodejs
+install npm
+move STUMAN folder in xampp > htdocs
+start apache and mysql from xampp control panel
+in a browser type "http://localhost/phpmyadmin/"
+create 2 new databases named "student_management" and "stumanlog"
+click on the created "student_management" database ,click on import tab, choose the file "personal_details.sql" file from "databases" folder of STUMAN and click "go" then import "subject_marks.sql" file from the same
+click on "stumanlog" database that you have created and import "stumanlog.sql" file from the "databases folder"
+open the STUMAN folder in VS code 
+open terminal in VS code 
+type cd front
+type "npm i --force" to download all dependencies
+To run the project: type "npm run start" and click on the link
---
+++ databases/stumanlog.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 10, 2024 at 09:06 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stumanlog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `username`, `password`) VALUES
+(5, 'helo@gmail.com', 'helo', '$2y$10$TfcCs8TKvFCRsMIHR/m72u6QmAeKlK0hg1TKTF0Xca94Mt5WvIi72'),
+(8, 'hello@gmail.com', 'hello', '$2y$10$NGvY05HA69vW86Rgh2SjWOa7nlHjl7sTxRvuBUHH8Te1s6EDIZdWG'),
+(9, 'test@gmail.com', 'testuser', '$2y$10$/pSguwTcbhvzl3th3/Sn8OrJ0s1VCNZRi3ULKYUggxp6.ecKm8KOC');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
